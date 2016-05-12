27882d898d264ffdeb616b110fabf03814ce3394
Hank Lidchi <hanklidchi@gmail.com>
2016-05-12 08:09:47 +0200

    Added panel to q2 and did some bootstrap cleaning up.

--- app/common/models/repos-model.js
+++ app/common/models/repos-model.js
@@ -1,19 +1,21 @@
 angular.module('gitApp.models.repos', [])
-    .service('ReposModel', ['$http',function($http) {
+    .service('ReposModel', ['$http', function($http) {
         var model = this,
-        	URLS = {
-        		FETCH: 'https://api.github.com/orgs/'
-        	},
+            URLS = {
+                FETCH: 'https://api.github.com/orgs/'
+            },
             repos;
-            function extract(result) {
-            	return result.data;
-            }
-            function cacheCategories(result) {
-            	categories = extract(result);
-            	return categories;
-            }
+
+        function extract(result) {
+            return result.data;
+        }
+
+        function cacheRepos(result) {
+            repos = extract(result);
+            return repos;
+        }
 
         model.getRepos = function(search) {
-            return $http.get(URLS.FETCH+search+'/repos').then(cacheCategories);
+            return $http.get(URLS.FETCH + search + '/repos').then(cacheRepos);
         }
     }]);
